96fbbfa6e0cb69dfb50e2929a422404cb17bf237
Amel MallemLouda <amelmalleml2016@gmail.com>
2020-12-05 20:30:55 -0500

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge_4")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ff6314b-875b-4baf-92d8-6e393966d7b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Challenge_4
+{
+
+    public abstract class IFamily
+    {
+        public string Address { get; }
+
+        public string FamilyName { get; }
+
+        public bool IsNice { get; set; }
+
+        public  string Nice();
+
+        public IFamily() { }
+
+        public IFamily(string address, string familyname, bool isnice)
+        {
+
+
+            Address = address;
+            FamilyName = familyname;
+            IsNice = isnice;
+        }
+
+    }
+
+
+
+    public class Father : IFamily
+    {
+
+
+        public string Address { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public bool IsNice { get; set; }
+
+        public bool IsPremium { get; set; }
+
+        public Father(bool isPrem, string address, string familyName, bool isNice)
+            : base(address, familyName, isNice)
+
+        {
+        IsPremium=isPrem;
+        }
+        public bool IsNice { get; set; }
+        public string Nice()
+        {
+            IsNice = true;
+            return "yes";
+        }
+    }
+
+    class Mother : IFamily
+    {
+
+        public string Address
+        {
+            get { return "54872 Barclay ct"; }
+        }
+        public string FamilyName
+        {
+            get { return "MallemLouda"; }
+        }
+
+        public bool IsNice { get; set; }
+
+        public string Nice()
+        {
+            IsNice = false;
+            return "The mother is rude sometimes";
+
+        }
+
+    }
+}
+
